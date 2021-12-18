b20b95c8374e8de196ed116845d65c6e001eac07
Ashokgithub1147 <ashokchowdaryulava@gmail.com>
2021-12-18 20:49:53 +0530

    updating changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var user=document.getElementById("user").value;
 var pas=document.getElementById("pass").value;
 if(user=="R161147"&&pass=="R161147"){
-	window.open('hi.html');
+	window.open('hi.htmll');
 }
 else{
 	alert("please enter correct username and passwprd");
